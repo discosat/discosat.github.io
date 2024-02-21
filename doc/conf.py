90fcbaa1535a1d838d86c306ec80a55ee33d0d88
Julian Priest <julianpriest@users.noreply.github.com>
2024-02-21 23:23:37 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
+project = 'Discosat'
+copyright = 'Discosat'
+author = 'Discosat'
 release = '0.1'
 
 
